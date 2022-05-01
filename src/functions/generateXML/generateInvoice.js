e368d19fc270a1d6a648c746a34bb63c596e18b5
Andrii Roizin <465182@student.saxion.nl>
2022-05-01 20:04:35 +0200

    table connected

--- src/functions/generateXML/generateInvoice.js
+++ src/functions/generateXML/generateInvoice.js
@@ -3,13 +3,32 @@ import listOfPurposesOfCallsConst from "../../config/consts/listOfPurposesOfCall
 
 const generateInvoice = (invoice) => {
 
+    let SupplyChainTradeTransaction = [];
+    for (let i = 0; i < invoice.tradeLineItems.length; i++) {
+        let tradeLineItem = invoice.tradeLineItems[i];
+        console.log("tradeLineItem",tradeLineItem);
+        let IncludedTradeLineItem = [];
+
+        let TradeProduct = [];
+        TradeProduct.push({Name: tradeLineItem.Name});
+
+        console.log("TradeProduct",TradeProduct)
+
+        IncludedTradeLineItem.push({TradeProduct: TradeProduct})
+        console.log("IncludedTradeLineItem",IncludedTradeLineItem)
+        SupplyChainTradeTransaction.push({IncludedTradeLineItem: IncludedTradeLineItem})
+        console.log("SupplyChainTradeTransaction",SupplyChainTradeTransaction)
+    }
+
+
     let invoiceXML = {
         CIIMessage:[{
             ExchangedDocument: [
                 {ID: invoice.id},
                 {IssueDateTime: invoice.occurrence}
-                ]
-        }]
+                ],
+
+        },{SupplyChainTradeTransaction:SupplyChainTradeTransaction}]
     }
 
     return invoiceXML;
