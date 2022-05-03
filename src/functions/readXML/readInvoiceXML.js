d48b06b9e7e62fa1c706592d93a16a5337f4ecec
Andrii Roizin <465182@student.saxion.nl>
2022-05-03 20:49:04 +0200

    reading invoice xml trade line items connected

--- src/functions/readXML/readInvoiceXML.js
+++ src/functions/readXML/readInvoiceXML.js
@@ -1,6 +1,25 @@
 const readInvoiceXML = (invoice, xml) => {
 
    invoice.invoice_id = xml.getElementsByTagName('ExchangedDocument')[0].children[0].value;
+   invoice.date_of_document = xml.getElementsByTagName('IssueDateTime')[0].value;
+
+   invoice.tradeLineItems = [];
+
+   let tradeLineItems = xml.getElementsByTagName('IncludedTradeLineItem');
+   for (let i = 0; i <tradeLineItems.length; i++) {
+
+      let tradeLineItem = tradeLineItems[i];
+
+      let tradeLineItemToSave = {
+         NR: i + 1,
+         Name: tradeLineItem.children[2].children[0].value
+      }
+
+      invoice.tradeLineItems.push(tradeLineItemToSave)
+      console.log("tradeLineItem ",tradeLineItem)
+
+   }
+   console.log("tradeLineItems ",tradeLineItems)
 };
 
 export default readInvoiceXML;
