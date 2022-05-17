ad3a86208b3ed94e1c0733ed57ca03c35cf6d9a7
Andrii Roizin <465182@student.saxion.nl>
2022-05-17 16:05:17 +0200

    Read CMR XML Document details

--- src/functions/readXML/readCmrXML.js
+++ src/functions/readXML/readCmrXML.js
@@ -2,6 +2,8 @@ import countryCodes from "../list_getters/countryCodes";
 
 const readCmrXML = (cmr, xml) => {
    cmr.cmr_id = xml.getElementsByTagName('eCMRID')[0].value;
+   cmr.date_of_document = xml.getElementsByTagName('eCMRIssueDate')[0].value;
+   cmr.remarks = xml.getElementsByTagName('ContractualRemarks')[0].value;
 };
 
 export default readCmrXML;
